7f135d8951d727a747201507fd3f84b8735ae205
Hamiz Ahmed <hamizahmed93@gmail.com>
2017-10-20 20:17:29 +0500

    Added derivative functions

--- test.py
+++ test.py
@@ -1,3 +1,342 @@
-import numpy
+import numpy as np
+import cPickle
+import os
+import gzip
 
-print "hello"
+
+def mnist(datasets_dir='./data'):
+    if not os.path.exists(datasets_dir):
+        os.mkdir(datasets_dir)
+    data_file = os.path.join(datasets_dir, 'mnist.pkl.gz')
+    if not os.path.exists(data_file):
+        print('... downloading MNIST from the web')
+        try:
+            import urllib
+            urllib.urlretrieve('http://google.com')
+        except AttributeError:
+            import urllib.request as urllib
+        url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+        urllib.urlretrieve(url, data_file)
+
+    print('... loading data')
+    # Load the dataset
+    f = gzip.open(data_file, 'rb')
+    try:
+        train_set, valid_set, test_set = cPickle.load(f, encoding="latin1")
+    except TypeError:
+        train_set, valid_set, test_set = cPickle.load(f)
+    f.close()
+
+    test_x, test_y = test_set
+    test_x = test_x.astype('float32')
+    test_x = test_x.astype('float32').reshape(test_x.shape[0], 1, 28, 28)
+    test_y = test_y.astype('int32')
+    valid_x, valid_y = valid_set
+    valid_x = valid_x.astype('float32')
+    valid_x = valid_x.astype('float32').reshape(valid_x.shape[0], 1, 28, 28)
+    valid_y = valid_y.astype('int32')
+    train_x, train_y = train_set
+    train_x = train_x.astype('float32').reshape(train_x.shape[0], 1, 28, 28)
+    train_y = train_y.astype('int32')
+    rval = [(train_x, train_y), (valid_x, valid_y), (test_x, test_y)]
+    print('... done loading data')
+    return rval
+
+
+# start by defining simple helpers
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+def sigmoid_d(x):
+    return np.exp(-x)/((1+np.exp(-x))**2)
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def tanh_d(x):
+    return 1.0 - np.tanh(x)**2
+
+
+def relu(x):
+    return np.maximum(0.0, x)
+
+
+def relu_d(x):
+    if x < 0:
+        return 0
+    return 1
+
+
+def softmax(x, axis=1):
+    # to make the softmax a "safe" operation we will
+    # first subtract the maximum along the specified axis
+    # so that np.exp(x) does not blow up!
+    # Note that this does not change the output.
+    x_max = np.max(x, axis=axis, keepdims=True)
+    x_safe = x - x_max
+    e_x = np.exp(x_safe)
+    return e_x / np.sum(e_x, axis=axis, keepdims=True)
+
+
+def one_hot(labels):
+    """this creates a one hot encoding from a flat vector:
+    i.e. given y = [0,2,1]
+     it creates y_one_hot = [[1,0,0], [0,0,1], [0,1,0]]
+    """
+    classes = np.unique(labels)
+    n_classes = classes.size
+    one_hot_labels = np.zeros(labels.shape + (n_classes,))
+    for c in classes:
+        one_hot_labels[labels == c, c] = 1
+    return one_hot_labels
+
+
+def unhot(one_hot_labels):
+    """ Invert a one hot encoding, creating a flat vector """
+    return np.argmax(one_hot_labels, axis=-1)
+
+
+# then define an activation function class
+class Activation(object):
+    def __init__(self, tname):
+        if tname == 'sigmoid':
+            self.act = sigmoid
+            self.act_d = sigmoid_d
+        elif tname == 'tanh':
+            self.act = tanh
+            self.act_d = tanh_d
+        elif tname == 'relu':
+            self.act = relu
+            self.act_d = relu_d
+        else:
+            raise ValueError('Invalid activation function.')
+
+    def fprop(self, input):
+        # we need to remember the last input
+        # so that we can calculate the derivative with respect
+        # to it later on
+        self.last_input = input
+        return self.act(input)
+
+    def bprop(self, output_grad):
+        return output_grad * self.act_d(self.last_input)
+
+
+# define a base class for layers
+class Layer(object):
+    def fprop(self, input):
+        """ Calculate layer output for given input 
+            (forward propagation). 
+        """
+        raise NotImplementedError('This is an interface class, please use a derived instance')
+
+    def bprop(self, output_grad):
+        """ Calculate input gradient and gradient 
+            with respect to weights and bias (backpropagation). 
+        """
+        raise NotImplementedError('This is an interface class, please use a derived instance')
+
+    def output_size(self):
+        """ Calculate size of this layer's output.
+        input_shape[0] is the number of samples in the input.
+        input_shape[1:] is the shape of the feature.
+        """
+        raise NotImplementedError('This is an interface class, please use a derived instance')
+
+
+# define a base class for loss outputs
+# an output layer can then simply be derived
+# from both Layer and Loss
+class Loss(object):
+    def loss(self, output, output_net):
+        """ Calculate mean loss given real output and network output. """
+        raise NotImplementedError('This is an interface class, please use a derived instance')
+
+    def input_grad(self, output, output_net):
+        """ Calculate input gradient real output and network output. """
+        raise NotImplementedError('This is an interface class, please use a derived instance')
+
+
+# define a base class for parameterized things
+class Parameterized(object):
+    def params(self):
+        """ Return parameters (by reference) """
+        raise NotImplementedError('This is an interface class, please use a derived instance')
+
+    def grad_params(self):
+        """ Return accumulated gradient with respect to params. """
+        raise NotImplementedError('This is an interface class, please use a derived instance')
+
+
+# define a container for providing input to the network
+class InputLayer(Layer):
+    def __init__(self, input_shape):
+        if not isinstance(input_shape, tuple):
+            raise ValueError("InputLayer requires input_shape as a tuple")
+        self.input_shape = input_shape
+
+    def output_size(self):
+        return self.input_shape
+
+    def fprop(self, input):
+        return input
+
+    def bprop(self, output_grad):
+        return output_grad
+
+
+class FullyConnectedLayer(Layer, Parameterized):
+    """ A standard fully connected hidden layer, as discussed in the lecture.
+    """
+
+    def __init__(self, input_layer, num_units,
+                 init_stddev, activation_fun=Activation('relu')):
+        self.num_units = num_units
+        self.activation_fun = activation_fun
+        # the input shape will be of size (batch_size, num_units_prev)
+        # where num_units_prev is the number of units in the input
+        # (previous) layer
+        self.input_shape = input_layer.output_size()
+        # TODO ################################
+        # TODO: implement weight initialization
+        # TODO ################################
+        # this is the weight matrix it should have shape: (num_units_prev, num_units)
+        self.W = np.random.randn(self.input_shape[1], num_units)  # FIXME
+        # and this is the bias vector of shape: (num_units)
+        self.b = np.zeros((1, num_units))  # FIXME
+        # create dummy variables for parameter gradients
+        # no need to change these here!
+        self.dW = None
+        self.db = None
+
+    def output_size(self):
+        return (self.input_shape[0], self.num_units)
+
+    def fprop(self, input):
+        # TODO ################################################
+        # TODO: implement forward propagation
+        # NOTE: you should also handle the case were
+        #       activation_fun is None (meaning no activation)
+        #       then this is simply a linear layer
+        # TODO ################################################
+        # you again want to cache the last_input for the bprop
+        # implementation below!
+        self.last_input = input
+        # FIXME
+        raise NotImplementedError("You should implement this!")
+        return np.zeros(self.output_size)
+
+    def bprop(self, output_grad):
+        """ Calculate input gradient (backpropagation). """
+        # TODO ################################
+        # TODO: implement backward propagation
+        # TODO ###############################
+
+        # HINT: you may have to divide the weights by n
+        #       to make gradient checking work
+        #       (since you want to divide the loss by number of inputs)
+        n = output_grad.shape[0]
+        # accumulate gradient wrt. the parameters first
+        # we will need to store these to later update
+        # the network after a few forward backward passes
+        # the gradient wrt. W should be stored as self.dW
+        # the gradient wrt. b should be stored as selfdb
+        raise NotImplementedError("you should implement this")
+        self.dW = None  # FIXME
+        self.db = None  # FIXME
+        # the gradient wrt. the input should be calculated here
+        grad_input = np.zeros_like(self.last_input)
+        return grad_input
+
+    def params(self):
+        return self.W, self.b
+
+    def grad_params(self):
+        return self.dW, self.db
+
+
+# finally we specify the interface for output layers
+# which are layers that also have a loss function
+# we will implement two output layers:
+#  a Linear, and Softmax (Logistic Regression) layer
+# The difference between output layers and and normal
+# layers is that they will be called to compute the gradient
+# of the loss through input_grad(). bprop will never
+# be called on them!
+class LinearOutput(Layer, Loss):
+    """ A simple linear output layer that  
+        uses a squared loss (e.g. should be used for regression)
+    """
+
+    def __init__(self, input_layer):
+        self.input_size = input_layer.output_size()
+
+    def output_size(self):
+        return (1,)
+
+    def fprop(self, input):
+        return input
+
+    def bprop(self, output_grad):
+        raise NotImplementedError(
+            'LinearOutput should only be used as the last layer of a Network'
+            + ' bprop() should thus never be called on it!'
+        )
+
+    def input_grad(self, Y, Y_pred):
+        # TODO ####################################
+        # TODO: implement gradient of squared loss
+        # TODO ####################################
+        return np.zeros_like(Y_pred)  # FIXME
+
+    def loss(self, Y, Y_pred):
+        loss = 0.5 * np.square(Y - Y_pred)
+        return np.mean(np.sum(loss, axis=1))
+
+
+class SoftmaxOutput(Layer, Loss):
+    """ A softmax output layer that calculates 
+        the negative log likelihood as loss
+        and should be used for classification.
+    """
+
+    def __init__(self, input_layer):
+        self.input_size = input_layer.output_size()
+
+    def output_size(self):
+        return (1,)
+
+    def fprop(self, input):
+        return softmax(input)
+
+    def bprop(self, output_grad):
+        raise NotImplementedError(
+            'SoftmaxOutput should only be used as the last layer of a Network'
+            + ' bprop() should thus never be called on it!'
+        )
+
+    def input_grad(self, Y, Y_pred):
+        # TODO #######################################################
+        # TODO: implement gradient of the negative log likelihood loss
+        # TODO #######################################################
+        # HINT: since this would involve taking the log
+        #       of the softmax (which is np.exp(x)/np.sum(x, axis=1))
+        #       this gradient computation can be simplified a lot!
+        return np.zeros_like(Y_pred)
+
+    def loss(self, Y, Y_pred):
+        # Assume one-hot encoding of Y
+        # calculate softmax first
+        out = softmax(Y_pred)
+        # to make the loss numerically stable
+        # you may want to add an epsilon in the log ;)
+        eps = 1e-10
+        # TODO ####################################
+        # calculate negative log likelihood
+        # TODO ####################################
+        loss = np.zeros_like(out)  # FIXME
+        return np.mean(loss)
+
